644a183e88bf96d49fb9ac6b6050a2c6a965e007
valberjr <valber@duck.com>
2024-06-26 16:17:34 -0300

    configure the job

--- src/main/java/com/example/billingjob/BillingJobConfiguration.java
+++ src/main/java/com/example/billingjob/BillingJobConfiguration.java
@@ -1,8 +1,14 @@
 package com.example.billingjob;
 
+import org.springframework.batch.core.Job;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BillingJobConfiguration {
-    // TODO add job definition here
+
+    @Bean
+    public Job job() {
+        return new BillingJob();
+    }
 }
