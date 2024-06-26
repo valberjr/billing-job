c5d279c3f523c54e581fd4bce1a5cd6a335de7d3
valberjr <valber@duck.com>
2024-06-26 15:50:58 -0300

    add BillingJobConfiguration file for billing job setup

---
+++ src/main/java/com/example/billingjob/BillingJobConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.billingjob;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BillingJobConfiguration {
+    // TODO add job definition here
+}
